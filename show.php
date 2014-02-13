9f84b7fe0d481088c43a2f935668bc49c4250357
mycodelobster <mycodelobster@outlook.com>
2014-02-13 21:50:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,2 +1,22 @@
-<?php 
-?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mockup</title>
+	<?php $this->load->view('layout/head') ?>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-3">
+				<?php $this->load->view('layout/sidebar') ?>
+			</div>
+			<div class="col-xs-9">
+				<div class="box">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
